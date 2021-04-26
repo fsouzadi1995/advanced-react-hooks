6783a70308c08661db4b94c110aa944f8290ce8d
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-04-26 11:18:02 -0300

    chore: exercise 03

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,40 +1,46 @@
 // useContext: simple Counter
 // http://localhost:3000/isolated/exercise/03.js
 
-import * as React from 'react'
+import * as React from 'react';
 
-// 🐨 create your CountContext here with React.createContext
+function useCount() {
+  const context = React.useContext(CountContext);
+  if (!context) {
+    throw new Error('useCount must be used inside CountContext');
+  }
 
-// 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+  const { count, setCount } = context;
 
+  return [count, setCount];
+}
+
+const CountContext = React.createContext();
+
+function CountProvider({ children }) {
+  const [count, setCount] = React.useState(0);
+
+  return <CountContext.Provider value={{ count, setCount }}>{children}</CountContext.Provider>;
+}
 function CountDisplay() {
-  // 🐨 get the count from useContext with the CountContext
-  const count = 0
-  return <div>{`The current count is ${count}`}</div>
+  const [count] = useCount();
+  return <div>{`The current count is ${count}`}</div>;
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
-  const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>Increment count</button>
+  const [setCount] = useCount().reverse();
+  const increment = () => setCount(c => c + 1);
+  return <button onClick={increment}>Increment count</button>;
 }
 
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
